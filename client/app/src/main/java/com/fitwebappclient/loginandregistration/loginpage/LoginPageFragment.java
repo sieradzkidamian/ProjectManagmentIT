ab08e73698e14f031c37db61a0160da450ae00ee
Kacper Przybylski <46407510+Kacper2401@users.noreply.github.com>
2023-04-22 10:35:41 +0200

    Sprint2/12 creating a project skeleton based on architecture   server side (#47)
    Co-authored-by: Mateusz Kucharek <m.kucharek18@gmail.com>

---
+++ client/app/src/main/java/com/fitwebappclient/loginandregistration/loginpage/LoginPageFragment.java
@@ -0,0 +1,49 @@
+package com.fitwebappclient.loginandregistration.loginpage;
+
+
+
+public class LoginPageFragment extends Fragment implements Serializable, Checker {
+    private LoginPageViewModel viewModel;
+    private ProgressDialog progressDialog;
+
+    public LoginPageFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+    }
+
+
+    }
+
+    private void handleLoginButton(View root) {
+
+    }
+
+    private void handleLoginAction(TextInputLayout userLoginLayout, TextInputLayout userPasswordLayout) {
+
+
+    }
+
+    private void showDialogWrongUserData() {
+
+    }
+
+    private void startAdminMainActivity(UserData userData) {
+
+    }
+
+    private void startUserMainActivity(UserData userData) {
+
+    }
+
+
+
